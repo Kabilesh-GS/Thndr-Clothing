d60a01cb00658255bbbe3f921a65a03eee23cf76
Kabilesh-GS <kabileshgs@gmail.com>
2025-05-08 09:45:09 +0530

    Installed SASS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
 function App() {
-  
+
+  const categories = [
+    {
+      name: 'Tees'
+    },
+    {
+      name: 'Pants'
+    },
+    {
+      name: 'Hats'
+    },
+    {
+      name: 'Shoes'
+    },
+    {
+      name: 'Shades'
+    },
+  ]
+
   return (
-    <>
-    </>
+    <div className="categories-container">
+      {
+        categories.map((e) => (
+          <div className="category-container">
+            {/* <img /> */}
+            <div className="category-body">
+              <h1>{e.name}</h1>
+              <p>Shop Now</p>
+            </div>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
