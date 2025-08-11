813447a6d92d34dd4e72d73433f6e51dde9338ce
Kabilesh-GS <kabileshgs@gmail.com>
2025-08-11 18:22:05 +0530

    About to complete checkout popup

--- src/Components/PopupCart/PopupCart.component.jsx
+++ src/Components/PopupCart/PopupCart.component.jsx
@@ -1,21 +1,39 @@
 import "./PopupCart.style.scss";
 
-const PopupCart = (props) => {
+const PopupCart = (props) => {  
+
+  let total = 0;
+  let totalDelivery = 0;
+  const calculateTotal = () => {
+    props.products.forEach((e) => (
+      total += (e.price * e.quantity)
+    ))
+  }
+  calculateTotal();
+
+  if(total<200) {
+    totalDelivery = total + 15;
+  };
+
   return props.trigger ? (
     <div className="popup">
       <div className="popupContainer">
         <h3>Check-Out Menu</h3>
-        <table className="items">
-          {props.products.map((e) => (
-            <tr>
-              <td>{e.name}</td>
-              <td>${e.price}</td>
-              <td>x</td>
-              <td>{e.quantity}</td>
-              <td className="totalPricePer">{(e.price * e.quantity).toFixed(2)}</td>
-            </tr>
-          ))}
-        </table>
+        {props.products.map((e) => (
+          <div className="items" key={e.id}>
+            <div>
+              <p className="name">{e.name}</p>
+              <div>
+                <p>$&nbsp;{e.price}</p>
+                <p>&nbsp; x &nbsp;</p>
+                <p>{e.quantity}</p>
+              </div>
+            </div>
+            <p>$&nbsp;{(e.price * e.quantity).toFixed(2)}</p>
+          </div>
+        ))}
+        {total < 200 ? <p className="deliveryHike">Delivery Charges Applied $15</p> : ""}
+        <p className="totalPrice">$&nbsp;{totalDelivery.toFixed(2)}</p>
         <button onClick={props.click}>X</button>
       </div>
     </div>
